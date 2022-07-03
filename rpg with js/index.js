eb38a266b1779d99cceebd3fecadffdf7b0a7a49
andrograf <patriciapusztai@gmail.com>
2022-07-03 23:03:48 +0200

    refactoring

--- rpg with js/index.js
+++ rpg with js/index.js
@@ -5,6 +5,43 @@ const canvas = document.querySelector('canvas') // kiválasztjuk a megfelelő ht
 canvas.width = 1024 //képernyő méretek
 canvas.height = 576
 
+const collisionMap = []
+for (let i = 0; i < collision.length; i+=60) {   // kétdimenziós collision lista készítése, hogy kezelhetőek legyen a térkép négyzetrácsa 
+    collisionMap.push(collision.slice(i,60 + i))
+}
+
+class Boundery {
+    static width = 64 // a sprite sheet mérete(16px) * 4, mert 400% zoomot alkalmaztunk
+    static height = 64
+    constructor({position}) {
+        this.position = position
+        this.width = 64
+        this.height = 64
+    }
+    draw() {
+        context.fillStyle = 'red'
+        context.fillRect(this.position.x,this.position.y, this.width,this.height) // collision megrajzolása, pirossal
+    }
+}
+
+const boundaries = []
+const offset = {
+    x: -2335,
+    y: -750
+}
+
+collisionMap.forEach((row,i) => {
+    row.forEach((symbol,j) => {
+        if (symbol === 401)           // ha a collicion lista eleme egyenlő 401-gyel, hozzáadja az új allistát az üres listába
+        boundaries.push(new Boundery({position: {  
+            x: j*Boundery.width + offset.x,
+            y: i*Boundery.height + offset.y
+        }}))
+    })
+})
+// console.log(boundaries)
+
+
 const context = canvas.getContext('2d') // 2d-s tartalmat fogunk megjeleníteni a canvas elemben
 context.fillStyle = 'white' // képernyő háttérszínének beállítása
 context.fillRect(0,0,canvas.width,canvas.height) // rect= rectengle, egy téglalap területet rajzol ki a megadott képernyőn - x,y koordináta, mint kezdőpont + a max. szélesség és magasság
@@ -26,17 +63,42 @@ playerImage.src = 'images/playerKuroko180percent.png' //player
 // játékos mozgásának létrehozása
 
 class Sprite {    // dinamikus koordináta megadás class segítségével
-    constructor({position,velocity,image}) {
+    constructor({position,velocity,image,frames = {max: 1}}) {
         this.position = position
         this.image = image
+        this.frame = frames
     }
     draw() {
         context.drawImage(this.image,this.position.x,this.position.y) // a map kezdőpontjának beállítása. Ha 0,0, akkor a kép bal felső sarkától fog rajzolni 
                                         // (tile-ból importálni a map-et megfelelő zoom méretben hozzá!!!)
+        context.drawImage(playerImage,
+            //képkivágás koordináták
+        
+            0, // eredet kép x koordinátája
+            0, // eredet kép y koordinátája
+            this.image.width/this.frame.max, // a kép kivágás szélességének a megadása - négy karakter egy sorban, mi egyet keresünk
+            this.image.height/this.frame.max,// a kép kivágás magasságának a megadása - négy karakter egy oszlopban, mi egyet keresünk
+            this.position.x,
+            this.position.y,
+            //aktuális koordináták
+
+            this.image.width/this.frame.max,
+            this.image.height/this.frame.max)
+                                        
     }
 }
+        
+// (),
+// , // karakter megrajzolása. Gyorsabban töltődik be, mint a héttér, ezért egy betöltés alatt kell mindkettőnek lefutnia
 
-const background = new Sprite({position: {x:-2335,y:-750},image:image})   // háttér osztály állandósítása
+const player = new Sprite({
+    position: {
+        x:canvas.width/2-(231/4)/2,
+        y:canvas.height/2},
+    image: playerImage,
+    frame: {max:4}})
+
+const background = new Sprite({position: {x: offset.x,y: offset.y},image:image})   // háttér osztály állandósítása
 
 const keys = {            // billentyű kulcsok, alapértelmezetten false 
     w: {
@@ -54,34 +116,34 @@ const keys = {            // billentyű kulcsok, alapértelmezetten false
 }
 
 
+const testBoundary = new Boundery( {
+    position : {
+        x: 400,
+        y: 400
+    }
+})
+
+const movables = [background, testBoundary] // az összes mozgó tárgy
 
 //animáció loop létrehozása
 
 function animate() {  
     window.requestAnimationFrame(animate) // recursive infinity loop létrehozása - végtelenül ismétli a benne lévő kódot(itt:képrajzolás)
     background.draw()
-
-    context.drawImage(playerImage,
-    //képkivágás koordináták
-
-    0, // eredet kép x koordinátája
-    0, // eredet kép y koordinátája
-    playerImage.width/4, // a kép kivágás szélességének a megadása - négy karakter egy sorban, mi egyet keresünk
-    playerImage.height/4,// a kép kivágás magasságának a megadása - négy karakter egy oszlopban, mi egyet keresünk
-
-    //aktuális koordináták
-
-    (canvas.width/2-(playerImage.width/4)/2),
-    canvas.height/2, // karakter megrajzolása. Gyorsabban töltődik be, mint a héttér, ezért egy betöltés alatt kell mindkettőnek lefutnia
-    playerImage.width/4,
-    playerImage.height/4)
+    // boundaries.forEach(boundary => {
+    //     boundary.draw()
+    // })
+    testBoundary.draw()
+    player.draw()
+    // ütközés létrehozása
+    //if (player.position.x + player.width)
 
 
     // karakter pozíció váltása, billentyű eseménykor
-    if (keys.w.pressed && lastKey === 'w')  background.position.y += 3   // ha lastKey megegyezik, abba az irányba mozdul el, hiába van még egy korábbi billenytű lenyomva
-    else if (keys.s.pressed && lastKey === 's')  background.position.y -= 3
-    else if (keys.a.pressed && lastKey === 'a')  background.position.x += 3
-    else if (keys.d.pressed && lastKey === 'd')  background.position.x -= 3
+    if (keys.w.pressed && lastKey === 'w')  { movables.forEach(movable => movable.position.y += 3)}  // ha lastKey megegyezik, abba az irányba mozdul el, hiába van még egy korábbi billenytű lenyomva
+    else if (keys.s.pressed && lastKey === 's')   { movables.forEach(movable => movable.position.y -= 3)}
+    else if (keys.a.pressed && lastKey === 'a')   { movables.forEach(movable => movable.position.x += 3)}
+    else if (keys.d.pressed && lastKey === 'd')   { movables.forEach(movable => movable.position.x -= 3)}
 }
 
 animate()
@@ -134,3 +196,5 @@ window.addEventListener('keyup',(e) => {      // felengedéskor visszatérnek fa
     console.log(keys)
 })
 // a rekurzív animate függvényben lévő feltételek miatt, az eventek újra és újra megismétlódnek, létrejön a mozgás - valójában a háttér mozog, nem a karakter!
+
+
